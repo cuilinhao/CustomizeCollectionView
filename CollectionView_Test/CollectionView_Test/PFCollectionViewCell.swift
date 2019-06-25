5b592aca6e48589cfcdd4f2e4eebb6267f788805
崔林豪 <15021538370@163.com>
2019-06-25 20:26:14 +0800

    update

--- CollectionView_Test/CollectionView_Test/PFCollectionViewCell.swift
+++ CollectionView_Test/CollectionView_Test/PFCollectionViewCell.swift
@@ -10,5 +10,25 @@ import UIKit
 
 
 class PFCollectionViewCell: UICollectionViewCell {
-    
+	
+	private lazy var lab : UILabel = {
+		
+		let lab = UILabel()
+		lab.frame = CGRect(x: 0, y: 0, width: self.bounds.size.width, height: 50)
+		lab.text = "rrrrr"
+		
+		lab.font = UIFont.systemFont(ofSize: 15)
+		lab.textAlignment = .center
+		
+		return lab
+	}()
+}
+
+
+extension PFCollectionViewCell {
+	
+	override func addSubview(_ view: UIView) {
+		
+		addSubview(lab)
+	}
 }
